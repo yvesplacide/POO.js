1e187d90f25d56314e14032fa92c78102f0a0cc5
Yves Placide <kouamekoffiyves57@gmail.com>
2024-12-29 07:16:04 +0000

    first commit

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ const personne2 = new personne ("Kalo",5)
 
 personne1.introduce()
 personne2.introduce()
+
+class employe extends personne{
+    constructor(jobtitle){
+        this.jobtitle = jobtitle;
+    }
+    Work(){
+        console.log(`${this.name} travaille en tant que ${jobtitle}`);
+    }
+}
