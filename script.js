07442dd380e35821cc13fe8304df3df210a11dc6
Yves Placide <kouamekoffiyves57@gmail.com>
2024-12-29 06:43:15 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+class personne{
+    constructor(name, age){ 
+this.name = name;
+this.age = age;
+}
+introduce(){
+    console.log(`je m'appelle ${this.name} et j'ai ${this.age}ans`)
+}
+}
+const personne1 = new personne("Yves", 10)
+const personne2 = new personne ("Kalo",5)
+
+personne1.introduce()
+personne2.introduce()
