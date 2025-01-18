258c9cd277f58fa7e23a820708e42b58f67ce3a5
Yves Placide <kouamekoffiyves57@gmail.com>
2025-01-18 12:33:36 +0000

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-/* exo 2  correction*/
+/* class personne {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  introduce() {
+    console.log(`je m'appelle ${this.name} et j'ai ${this.age}ans`);
+  }
+}
+const personne1 = new personne("Yves", 10);
+const personne2 = new personne("Kalo", 5);
+
+personne1.introduce();
+personne2.introduce();
+
+class employe extends personne {
+  constructor(jobtitle) {
+    this.jobtitle = jobtitle;
+  }
+  Work() {
+    console.log(`${this.name} travaille en tant que ${jobtitle}`);
+  }
+} */
+/* exo whatssap */
+/* exo 1  correction*/
 class BanKAccount {
   #Balance;
   constructor(initial) {
@@ -18,7 +42,31 @@ class BanKAccount {
     console.log(`Solde actuel: ${this.#Balance}`);
   }
 }
-const Banque1 = new BanKAccount(100);
-Banque1.deposit(100);
-Banque1.withdraw(1500);
-Banque1.getBalance();
+const banque1 = new BanKAccount(100);
+banque1.deposit(2000);
+banque1.withdraw(1500);
+banque1.getBalance();
+
+/* exo2 */
+//Créez une classe parent Animal avec Une propriété name.
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  //Une méthode makeSound() qui affiche : "[Nom] fait un bruit."
+  makeSound() {
+    console.log(`${this.name} fait un bruit.`);
+  }
+}
+//Créez une classe enfant Dog qui hérite de la classe Animal et redefinissez la méthode makeSound() pour afficher : "[Nom] aboie."
+class Dog extends Animal {
+  makeSound() {
+    console.log(`${this.name} aboie.`);
+    console.log("Je suis un chien");
+  }
+}
+//Créez une instance de chaque classe et appelez leurs méthodes.
+const animal = new Animal("Animal");
+animal.makeSound();
+const dog = new Dog("Dog");
+dog.makeSound();
